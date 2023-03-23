d16f05036ff4fd802794d05acd14423f7d9bcf5b
Key5n <k.nogami.472@stn.nitech.ac.jp>
2023-03-24 03:41:04 +0900

    fix: fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Key5n.github.io",
+  site: "https://key5n.github.io",
   base: "/astro-club-site",
 });
